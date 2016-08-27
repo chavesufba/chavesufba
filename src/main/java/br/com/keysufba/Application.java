16bfe33c9dfd54315312e9bfcad1c33ed8c1cc7f
Victor Martinez <vcrmartinez@gmail.com>
2016-08-27 14:43:04 -0300

    Enable auto configuration

--- src/main/java/br/com/keysufba/Application.java
+++ src/main/java/br/com/keysufba/Application.java
@@ -1,9 +1,11 @@
 package br.com.keysufba;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+@EnableAutoConfiguration
 @SpringBootApplication
 public class Application {
 
