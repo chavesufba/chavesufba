04f3eef82ca46c9d4d4669c346499c54d05d469c
Victor Martinez <vcrmartinez@gmail.com>
2016-08-27 23:38:48 -0300

    Change Application annotations

--- src/main/java/br/com/keysufba/Application.java
+++ src/main/java/br/com/keysufba/Application.java
@@ -2,11 +2,12 @@ package br.com.keysufba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-
+@Configuration
 @EnableAutoConfiguration
-@SpringBootApplication
+@ComponentScan
 public class Application {
 
   public static void main(String[] args) {
