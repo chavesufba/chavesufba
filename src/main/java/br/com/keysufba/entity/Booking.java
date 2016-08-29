d62967f52ecd30eff6ac8083a794dba5bebdbb93
Icaro Erasmo Souza Barreiro <erasmo.iesb@gmail.com>
2016-08-29 14:07:56 -0300

    alterações na classe Booking

--- src/main/java/br/com/keysufba/entity/Booking.java
+++ src/main/java/br/com/keysufba/entity/Booking.java
@@ -3,22 +3,51 @@ package br.com.keysufba.entity;
 import java.util.Calendar;
 
 import javax.persistence.Column;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 public class Booking {
+	
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Integer id;
 	
-	@Column(name="professor_Id")
+	
+	@OneToMany
+	@JoinColumn(name="professor_Id")
 	private Teacher teacherId;
+	
+	
+	@OneToOne
+	@JoinColumn(name="room_id", nullable=false)
 	private Room roomId;
+	
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name="hora_inicio", nullable=false)
 	private Calendar beginTime;
+	
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name="hora_fim", nullable=false)
 	private Calendar endTime;
+	
+	
+	@OneToOne
+	@JoinColumn(name="tecnico_id", nullable=false, foreignKey=@ForeignKey(name="id"))
 	private Technician technicianId;
+	
+	
+	@OneToOne
+	@JoinColumn(name="status_reserva", nullable=false, foreignKey=@ForeignKey(name="id"))
 	private BookingStatus bookingStatusId;
 	
 }
