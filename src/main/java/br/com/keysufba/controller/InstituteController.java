570c25493634fbf562e1cea50b577880729c1925
Victor Martinez <vcrmartinez@gmail.com>
2016-09-04 16:05:10 -0300

    Implement CRUD of Institutes

--- src/main/java/br/com/keysufba/controller/InstituteController.java
+++ src/main/java/br/com/keysufba/controller/InstituteController.java
@@ -2,6 +2,8 @@ package br.com.keysufba.controller;
 
 import java.util.List;
 
+import br.com.keysufba.entity.Institute;
+import br.com.keysufba.service.InstituteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpEntity;
@@ -14,29 +16,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.keysufba.entity.Room;
-import br.com.keysufba.service.RoomService;
-
 @RestController
-@RequestMapping("/api/v1/rooms")
-public class RoomController {
+@RequestMapping("/api/v1/institutes")
+public class InstituteController {
 
   @Autowired
-  private RoomService roomService;
+  private InstituteService instituteService;
 
   @RequestMapping(method = RequestMethod.GET)
-  public HttpEntity<List<Room>> getRooms() {
-    final List<Room> rooms = roomService.findAll();
+  public HttpEntity<List<Institute>> getInstitutes() {
+    final List<Institute> rooms = instituteService.findAll();
     return new ResponseEntity<>(rooms, HttpStatus.OK);
   }
 
   @RequestMapping(path = "/{id}", method = RequestMethod.GET)
-  public HttpEntity<Room> getRoom(@PathVariable("id") Integer id) {
+  public HttpEntity<Institute> getInstitute(@PathVariable("id") Integer id) {
     if (id == null) {
       return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    final Room room = roomService.findById(id);
+    final Institute room = instituteService.findById(id);
     if (room == null) {
       return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
@@ -44,13 +43,13 @@ public class RoomController {
   }
 
   @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-  public HttpEntity<Room> createRoom(@RequestBody Room room) throws DataIntegrityViolationException {
+  public HttpEntity<Institute> createInstitute(@RequestBody Institute room) throws DataIntegrityViolationException {
     if (room == null) {
       return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
     try {
-      roomService.create(room);
+      instituteService.create(room);
       return new ResponseEntity<>(room, HttpStatus.OK);
     } catch (Exception e) {
       return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
@@ -58,35 +57,34 @@ public class RoomController {
   }
 
   @RequestMapping(path = "/{id}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-  public HttpEntity<Room> updateRoom(@PathVariable("id") Integer id, @RequestBody Room room) {
+  public HttpEntity<Institute> updateInstitute(@PathVariable("id") Integer id, @RequestBody Institute room) {
     if (id == null || room == null) {
       return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
     try {
       room.setId(id);
-      final Room updatedRoom = roomService.update(room);
-      if (updatedRoom == null) {
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+      final Institute updatedInstitute = instituteService.update(room);
+      if (updatedInstitute == null) {
+        new ResponseEntity<>(HttpStatus.NOT_FOUND);
       }
-      return new ResponseEntity<>(updatedRoom, HttpStatus.OK);
+      return new ResponseEntity<>(updatedInstitute, HttpStatus.OK);
     } catch (Exception e) {
       return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
   }
 
   @RequestMapping(path = "/{id}", method = RequestMethod.DELETE, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-  public HttpEntity<Room> deleteRoom(@PathVariable("id") Integer id) {
+  public HttpEntity<Institute> deleteInstitute(@PathVariable("id") Integer id) {
     if (id == null) {
       return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
     try {
-      final Integer deletedId = roomService.delete(id);
-      return new ResponseEntity<>(new Room(deletedId), HttpStatus.OK);
+      final Integer deletedId = instituteService.delete(id);
+      return new ResponseEntity<>(new Institute(deletedId), HttpStatus.OK);
     } catch (Exception e) {
       return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
   }
-
 }
