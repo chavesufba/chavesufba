9b897715f9c0aecf7db93de8da6527e57c4755c8
Victor Martinez <vcrmartinez@gmail.com>
2016-09-04 12:18:37 -0300

    Implement service

---
+++ src/main/java/br/com/keysufba/service/PavilionService.java
@@ -0,0 +1,44 @@
+package br.com.keysufba.service;
+
+import java.util.List;
+
+import br.com.keysufba.entity.Pavilion;
+import br.com.keysufba.repository.PavilionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("pavilionService")
+public class PavilionService implements GenericService<Pavilion> {
+
+  @Autowired
+  private PavilionRepository pavilionRepository;
+
+  @Override
+  public List<Pavilion> findAll() {
+    return pavilionRepository.findAll();
+  }
+
+  @Override
+  public Pavilion findById(Integer id) {
+    return pavilionRepository.findOne(id);
+  }
+
+  @Override
+  public Pavilion create(Pavilion p) {
+    pavilionRepository.save(p);
+    return p;
+  }
+
+  @Override
+  public Integer delete(Pavilion p) {
+    pavilionRepository.delete(p);
+    return null;
+  }
+
+  @Override
+  public Pavilion update(Pavilion p) {
+    pavilionRepository.save(p);
+    return null;
+  }
+
+}
