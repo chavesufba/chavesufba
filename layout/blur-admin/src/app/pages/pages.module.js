2745af7392eb6f9b8c18bec2fafc8ac59c40de2f
Lucas Ayres <lucasayres@startonapp.com>
2016-09-28 19:58:38 -0300

    Adicionando novo layout blur-admin em angularjs + documentação

---
+++ layout/blur-admin/src/app/pages/pages.module.js
@@ -0,0 +1,34 @@
+/**
+ * @author v.lugovsky
+ * created on 16.12.2015
+ */
+(function () {
+  'use strict';
+
+  angular.module('BlurAdmin.pages', [
+    'ui.router',
+
+    'BlurAdmin.pages.dashboard',
+    'BlurAdmin.pages.ui',
+    'BlurAdmin.pages.form',
+    'BlurAdmin.pages.tables',
+    'BlurAdmin.pages.charts',
+    'BlurAdmin.pages.profile'
+  ])
+      .config(routeConfig);
+
+  /** @ngInject */
+  function routeConfig($urlRouterProvider, baSidebarServiceProvider) {
+    $urlRouterProvider.otherwise('/dashboard');
+
+    baSidebarServiceProvider.addStaticItem({
+      title: 'Conta',
+      icon: 'ion-document',
+      subMenu: [{
+        title: 'Perfil',
+        stateRef: 'profile'
+      }]
+    });
+  }
+
+})();
