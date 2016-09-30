df42a79bf58e3ea6983bd21673b7347b944ad4c5
Naara <naara.musse@simetrianet.com.br>
2016-09-30 14:22:30 -0300

    cadastro de sala em angular

--- layout/blur-admin/src/app/pages/salas/SalasPageCtrl.js
+++ layout/blur-admin/src/app/pages/salas/SalasPageCtrl.js
@@ -1,94 +1,98 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  angular.module('BlurAdmin.pages.salas')
-      .controller('BuscarSalasPageCtrl', BuscarSalasPageCtrl)
-      .controller('VisualizarSalaPageCtrl', VisualizarSalaPageCtrl);
+    angular.module('BlurAdmin.pages.salas')
+        .controller('BuscarSalasPageCtrl', BuscarSalasPageCtrl)
+        .controller('VisualizarSalaPageCtrl', VisualizarSalaPageCtrl)
+        .controller('CadastrarSalaPageCtrl', CadastrarSalaPageCtrl);
 
-  /** @ngInject */
-  function BuscarSalasPageCtrl($scope, $filter, $location, editableOptions, editableThemes) {
+    /** @ngInject */
+    function BuscarSalasPageCtrl($scope, $filter, $location, editableOptions, editableThemes) {
 
-    $scope.smartTableData = [
-      {
-        numero: 133,
-        local: 'IM',
-        tipo: 'Laboratório',
-        cadeiras: 40,
-      },
-      {
-        numero: 133,
-        local: 'IM',
-        tipo: 'Laboratório',
-        cadeiras: 40,
-      },
-      {
-        numero: 133,
-        local: 'IM',
-        tipo: 'Laboratório',
-        cadeiras: 40,
-      },
-      {
-        numero: 133,
-        local: 'IM',
-        tipo: 'Laboratório',
-        cadeiras: 40,
-      },
-      {
-        numero: 133,
-        local: 'IM',
-        tipo: 'Laboratório',
-        cadeiras: 40,
-      },
-      {
-        numero: 133,
-        local: 'IM',
-        tipo: 'Laboratório',
-        cadeiras: 40,
-      },
-      {
-        numero: 133,
-        local: 'IM',
-        tipo: 'Laboratório',
-        cadeiras: 40,
-      }
-    ];
+        $scope.smartTableData = [
+            {
+                numero: 133,
+                local: 'IM',
+                tipo: 'Laboratório',
+                cadeiras: 40,
+            },
+            {
+                numero: 133,
+                local: 'IM',
+                tipo: 'Laboratório',
+                cadeiras: 40,
+            },
+            {
+                numero: 133,
+                local: 'IM',
+                tipo: 'Laboratório',
+                cadeiras: 40,
+            },
+            {
+                numero: 133,
+                local: 'IM',
+                tipo: 'Laboratório',
+                cadeiras: 40,
+            },
+            {
+                numero: 133,
+                local: 'IM',
+                tipo: 'Laboratório',
+                cadeiras: 40,
+            },
+            {
+                numero: 133,
+                local: 'IM',
+                tipo: 'Laboratório',
+                cadeiras: 40,
+            },
+            {
+                numero: 133,
+                local: 'IM',
+                tipo: 'Laboratório',
+                cadeiras: 40,
+            }
+        ];
 
-    $scope.redirect_relatorio = function ( id_sala ) {
-        window.location.href = '#/salas/visualizar_sala#' + String(id_sala);
+        $scope.redirect_relatorio = function (id_sala) {
+            window.location.href = '#/salas/visualizar_sala#' + String(id_sala);
+        }
+
+        $scope.salas = $scope.smartTableData.slice(0, 36);
+        editableOptions.theme = 'bs3';
     }
 
-    $scope.salas = $scope.smartTableData.slice(0, 36);
-    editableOptions.theme = 'bs3';
-  }
-
-  function VisualizarSalaPageCtrl($scope, $filter, $location, editableOptions, editableThemes, toastr) {
+    function VisualizarSalaPageCtrl($scope, $filter, $location, editableOptions, editableThemes, toastr) {
 
 
+        $scope.get_sala = function () {
+            $scope.id = $location.hash()
+        }
 
-    $scope.get_sala = function(){
-        $scope.id = $location.hash()
+        $scope.get_status_form = function () {
+            $scope.disabledField = true; // desabilitar campo para edição
+            $scope.nameButton = "Alterar Reserva"
+        }
+
+        $scope.update_reserva = function () {
+            if ($scope.disabledField) {
+                $scope.disabledField = false;
+                $scope.nameButton = "Salvar Reserva"
+            } else {
+                $scope.disabledField = true;
+                toastr.success('As informações foram salvas com sucesso!');
+                $scope.nameButton = "Alterar Reserva"
+            }
+        }
+
+        $scope.isDisabledDate = function (currentDate, mode) {
+            return mode === 'day' && (currentDate.getDay() === 0 || currentDate.getDay() === 6);
+        };
+
     }
 
-    $scope.get_status_form = function(){
-      $scope.disabledField = true; // desabilitar campo para edição
-      $scope.nameButton = "Alterar Reserva"
+    function CadastrarSalaPageCtrl($scope, $filter, $location, editableOptions, editableThemes, toastr) {
+        return true;
     }
-
-    $scope.update_reserva = function(){
-      if ($scope.disabledField) {
-        $scope.disabledField = false;
-        $scope.nameButton = "Salvar Reserva"
-      } else {
-        $scope.disabledField = true;
-        toastr.success('As informações foram salvas com sucesso!');
-        $scope.nameButton = "Alterar Reserva"
-      }
-    }
-
-    $scope.isDisabledDate = function(currentDate, mode) {
-      return mode === 'day' && (currentDate.getDay() === 0 || currentDate.getDay() === 6);
-    };
-    
-  }
 
 })();
--- layout/blur-admin/src/app/pages/salas/salas.module.js
+++ layout/blur-admin/src/app/pages/salas/salas.module.js
@@ -3,38 +3,46 @@
  * created on 16.12.2015
  */
 (function () {
-  'use strict';
+    'use strict';
 
-  angular.module('BlurAdmin.pages.salas', [])
-    .config(routeConfig);
+    angular.module('BlurAdmin.pages.salas', [])
+        .config(routeConfig);
 
-  /** @ngInject */
-  function routeConfig($stateProvider, $urlRouterProvider) {
-    $stateProvider
-        .state('salas', {
-          url: '/salas',
-          template : '<ui-view></ui-view>',
-          abstract: true,
-          title: 'Salas',
-          sidebarMeta: {
-            icon: 'ion-ios-book',
-            order: 300,
-          },
-	  }).state('salas.buscar_salas', {
-          url: '/buscar_salas',
-          templateUrl: 'app/pages/salas/buscar_salas/buscar_salas.html',
-          title: 'Buscar Salas',
-          controller: 'BuscarSalasPageCtrl',
-          sidebarMeta: {
-            order: 300,
-          },
-      }).state('salas.visualizar_sala', {
-          url: '/visualizar_sala',
-          templateUrl: 'app/pages/salas/visualizar_sala/visualizar_sala.html',
-          title: 'Visualizar Sala',
-          controller: 'VisualizarSalaPageCtrl'
-        });
-    $urlRouterProvider.when('/salas','/salas/buscar_salas');
-  }
+    /** @ngInject */
+    function routeConfig($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('salas', {
+                url: '/salas',
+                template: '<ui-view></ui-view>',
+                abstract: true,
+                title: 'Salas',
+                sidebarMeta: {
+                    icon: 'ion-ios-book',
+                    order: 300,
+                },
+            }).state('salas.buscar_salas', {
+                url: '/buscar_salas',
+                templateUrl: 'app/pages/salas/buscar_salas/buscar_salas.html',
+                title: 'Buscar Salas',
+                controller: 'BuscarSalasPageCtrl',
+                sidebarMeta: {
+                    order: 300,
+                },
+            }).state('salas.cadastrar_sala', {
+                url: '/cadastrar_sala',
+                templateUrl: 'app/pages/salas/cadastrar_sala/cadastrar_sala.html',
+                title: 'Cadastrar Sala',
+                controller: 'CadastrarSalaPageCtrl',
+                sidebarMeta: {
+                    order: 300,
+                },
+            }).state('salas.visualizar_sala', {
+                url: '/visualizar_sala',
+                templateUrl: 'app/pages/salas/visualizar_sala/visualizar_sala.html',
+                title: 'Visualizar Sala',
+                controller: 'VisualizarSalaPageCtrl'
+            });
+        $urlRouterProvider.when('/salas', '/salas/buscar_salas');
+    }
 
 })();
